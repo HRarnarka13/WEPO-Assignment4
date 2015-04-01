67a7903a460be673a5d9c64b9c3baa69ea926f16
Arnar Kari <arnarkari93@gmail.com>
2015-03-31 23:48:00 -0700

    Finally the pipies are generating and moving. Next up: create a random number generator for gap and collision check

--- app/scripts/pipe.js
+++ app/scripts/pipe.js
@@ -1,27 +1,46 @@
 window.Pipe = (function() {
     'use strict';
 
-    var Pipe = function(el, game) {
-    	this.el = el;
+    var GAPHEIGHT = 15;
+
+    var Pipe = function(topHeight, id, game) {
+    	this.topHeight = topHeight;
+    	this.id = id;
     	this.game = game;
-    	this.top = { x: 102.4, y: 21.6 };
-    	this.bottom = { x:102.4, y: 43.2 };
+    	this.top = { x: 102.4, y: topHeight };
+    	var gap = 40 - (topHeight + GAPHEIGHT);
+    	this.bottom = { x:102.4, y: gap };
+
+
+    	// Create a top an bottom pipe
+    	var top = "<div style='height: " + this.top.y + "em' class='Toppipe'></div>"
+    	var bot = "<div style='margin-top: " + this.bottom.y + "em; height: " + 100 + "em' class='Bottompipe'></div>"
+
+    	// Append the pipes to the list of pipes
+    	$('#'+ this.id).append(top);
+    	$('#'+ this.id).append(bot);
 	};
 
 	Pipe.prototype.onFrame = function(delta) {
 
-		if (this.x < 0) {
-			this.x = 100;
+		if (this.top.x < -20) {
+			this.deletePipe(this.id); // delete the pipe if it has passed by
+		} else if ( this.top.x <= 42) {
+			$('.score').text(this.id); // update the score
 		}
 		this.top.x -= 0.4;
 
-		// console.log(this.top.x);
-		this.el.css('transform', 'translate3d(' + this.top.x + 'em, 0em, 0em)');
+		//Update UI
+		$('#'+ this.id).css('transform', 'translate3d(' + this.top.x + 'em, 0em, 0em)');
 	};
 
-	Pipe.prototype.generatePipes = function(first_argument) {
+	Pipe.prototype.checkCollision = function(player) {
 
 	};
+
+	Pipe.prototype.deletePipe = function (id) {
+    	$('#' + id).remove();
+    };
 
 	Pipe.prototype.method_name = function(first_argument) {
 		// body...
---
+++ app/scripts/pipes.js
@@ -0,0 +1,40 @@
+window.Pipes = (function() {
+    'use strict';
+
+    var Pipes = function (el, game) {
+    	this.el = el;
+    	this.game = game;
+    	this.pipes = [];
+    	this.pipeID = 0;
+    }
+
+    Pipes.prototype.onFrame = function(delta) {
+    	for (var i = 0; i < this.pipes.length; i++) {
+    		this.pipes[i].onFrame(delta);
+    	};
+    };
+
+    Pipes.prototype.generatePipes = function() {
+
+    	this.pipeID++;
+
+    	// Create div which contains the top and bottom pipe
+    	var newpipe = "<div class='Pipe' id='" + this.pipeID + "'></div>"
+    	$('.Pipes').append(newpipe);
+
+    	// TODO: Generate a random number
+    	var pipeHeight = 10;
+
+
+    	// Create the pipes and add them to the list
+    	this.pipes.push(new window.Pipe(pipeHeight, this.pipeID, this.game));
+    };
+
+    Pipes.prototype.checkCollision = function (player) {
+    	// TODO : implement
+    	return false;
+    };
+
+    return Pipes;
+
+})();
