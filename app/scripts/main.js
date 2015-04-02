e95bcc784d531d95cae403a63919d916a7d59b9a
Arnar Kari <arnarkari93@gmail.com>
2015-04-01 17:24:29 -0700

    responsive

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,5 +5,13 @@ $(function() {
     'use strict';
 
     var game = new window.Game($('.GameCanvas'));
+
+    var fontSize = Math.min(window.innerWidth / 102.4, window.innerHeight / 57.6);
+    if (fontSize < 10) {
+    	$('.GameCanvas').css('fontSize', fontSize + 'px');
+	}
+    $('.GameCanvas').css('background-position-y', 52 - (fontSize/10) + 'em');
     game.start();
+
+
 });
